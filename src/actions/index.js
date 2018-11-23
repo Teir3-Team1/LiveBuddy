f2602f78659f286781d587019c813562b1ffa2ba
Ahmed Abdelhamid <slevin.glevra@gmail.com>
2018-11-23 09:29:37 +0300

    Set session persistance to session

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,20 @@
-import { auth } from '../config/firebase';
+import firebase, { auth } from '../config/firebase';
 import { LOGIN, LOGOUT } from './types';
 import { existingEmailError } from '../functions/authFunctions';
 
 export const login = provider => dispatch => {
-  auth
-    .signInWithPopup(provider)
-    .then(({ user, credential }) => {
-      auth.signInAndRetrieveDataWithCredential(credential);
-      dispatch({
-        type: LOGIN,
-        userId: user.uid
-      });
-    })
-    .catch(error => existingEmailError(error));
+  auth.setPersistence(firebase.auth.Auth.Persistence.SESSION).then(() => {
+    auth
+      .signInWithPopup(provider)
+      .then(({ user, credential }) => {
+        auth.signInAndRetrieveDataWithCredential(credential);
+        dispatch({
+          type: LOGIN,
+          userId: user.uid
+        });
+      })
+      .catch(error => existingEmailError(error));
+  });
 };
 
 export const logout = () => dispatch => {
