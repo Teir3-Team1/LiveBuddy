5d0f0a491ecef83fddb5fb9b325cbd37eee41f71
Ahmed Abdelhamid <slevin.glevra@gmail.com>
2018-11-24 13:56:56 +0300

    Finish handle account exists wuth different credential error

--- src/functions/authFunctions.js
+++ src/functions/authFunctions.js
@@ -1,19 +1,24 @@
 import { auth } from '../config/firebase';
 import { providers } from '../config/firebase';
 
-export const existingEmailError = ({ code, email, credential }) => {
+export const existingEmailError = ({
+  code,
+  email,
+  credential: pendingCred
+}) => {
   if (code === 'auth/account-exists-with-different-credential') {
     auth.fetchSignInMethodsForEmail(email).then(methods => {
       const authProvider = providers.filter(
         provider => provider.providerId === methods[0]
       );
       auth
-        .signInWithRedirect(authProvider[0])
-        .then(user => user.linkAndRetrieveDataWithCredential(credential))
-        .then(result => {
-          console.log(result);
+        .signInWithPopup(authProvider[0])
+        .then(({ user }) => {
+          user.linkAndRetrieveDataWithCredential(pendingCred);
         })
-        .catch(error => console.log('Error: ', error));
+        .catch(error => {
+          console.log('Error: ', error.code);
+        });
     });
   }
 };
